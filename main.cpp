74d2acf95e36e7f2ea10d8d27755114220acacb9
KonaeAkira <longtruong2411@gmail.com>
2020-02-17 17:48:25 +0100

    Optimizations Interweaved constant arrays to improve caching performance Adjusted segment size Removed costly loop when initializing sieve primes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <bitset>
 #include <algorithm>
 #include <thread>
-#include <mutex>
+#include <atomic>
 #include <chrono>
 
 #include "number.hpp"
@@ -17,7 +17,9 @@ const uint64_t SIEVE_LIM_SQRT = std::sqrt(SIEVE_LIM);
 
 const uint32_t THREADS = std::thread::hardware_concurrency();
 
-const uint64_t SEGMENT_SIZE = 1 << 11;
+const uint64_t SEGMENT_SIZE = 1 << 13;
+
+std::atomic<uint64_t> global_count = 3;
 
 void presieve(std::vector<number> &primes)
 {
@@ -32,7 +34,7 @@ void presieve(std::vector<number> &primes)
 		}
 }
 
-void sieve(const std::vector<number> &primes, const uint64_t begin_segment, const uint64_t end_segment, uint64_t &global_count, std::mutex &mutex)
+void sieve(const std::vector<number> &primes, const uint64_t begin_segment, const uint64_t end_segment)
 {
 	uint64_t internal_count = 0;
 	std::vector<number> bucket;
@@ -40,10 +42,11 @@ void sieve(const std::vector<number> &primes, const uint64_t begin_segment, cons
 	for (const number prime : primes)
 		bucket.emplace_back(prime.get_number(), begin_segment * SEGMENT_SIZE * 210);
 	std::bitset<SEGMENT_SIZE * 48> bitset;
+	uint64_t begin_block, end_block;
 	for (uint64_t segment = begin_segment; segment < end_segment; ++segment)
 	{
-		const uint64_t begin_block = segment * SEGMENT_SIZE;
-		const uint64_t end_block = (segment + 1) * SEGMENT_SIZE;
+		begin_block = segment * SEGMENT_SIZE;
+		end_block = (segment + 1) * SEGMENT_SIZE;
 		bitset.reset();
 		for (uint32_t i = 0; i < primes.size(); ++i)
 			while (bucket[i].get_block() < end_block)
@@ -55,15 +58,13 @@ void sieve(const std::vector<number> &primes, const uint64_t begin_segment, cons
 			internal_count += bitset.size() - bitset.count();
 		else
 		{
-			const uint16_t value[48] = {1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 121, 127, 131, 137, 139, 143, 149, 151, 157, 163, 167, 169, 173, 179, 181, 187, 191, 193, 197, 199, 209};
+			const uint8_t value[48] = {1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 121, 127, 131, 137, 139, 143, 149, 151, 157, 163, 167, 169, 173, 179, 181, 187, 191, 193, 197, 199, 209};
 			for (uint64_t i = 0; (begin_block + i / 48) * 210 + value[i % 48] < SIEVE_LIM; ++i)
 				internal_count += !bitset.test(i);
 			break;
 		}
 	}
-	mutex.lock();
 	global_count += internal_count;
-	mutex.unlock();
 }
 
 int main()
@@ -78,9 +79,6 @@ int main()
 	std::vector<number> primes;
 	presieve(primes);
 	
-	uint64_t prime_count = 3;
-	std::mutex prime_count_mutex;
-	
 	// spawn workers
 	std::vector<std::thread*> threads;
 	uint64_t segment_count = (SIEVE_LIM - 1) / (SEGMENT_SIZE * 210) + 1;
@@ -88,7 +86,7 @@ int main()
 	{
 		uint64_t start_segment = segment_count - (segment_count / (THREADS - threads.size()));
 		printf("  New thread %04lld-%04lld\n", start_segment, segment_count);
-		threads.push_back(new std::thread(sieve, std::ref(primes), start_segment, segment_count, std::ref(prime_count), std::ref(prime_count_mutex)));
+		threads.push_back(new std::thread(sieve, std::ref(primes), start_segment, segment_count));
 		segment_count = start_segment;
 	}
 	for (auto thread : threads)
@@ -97,7 +95,7 @@ int main()
 	// stop timer
 	std::chrono::high_resolution_clock::time_point stop_time = std::chrono::high_resolution_clock::now();
 	
-	printf("Counted %lld primes!\n", prime_count);
+	printf("Counted %lld primes!\n", global_count.load());
 	printf("Elapsed time: %d ms\n", std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time).count() / 1000);
 	return 0;
 }
